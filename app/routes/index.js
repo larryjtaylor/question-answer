756dcba8823400fc4b45c6be37466bfabd9735d8
Larry <larryjamestaylor@gmail.com>
2017-08-11 11:50:39 -0700

    build qquestion model and display in route handler

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  body: "What language should I start with? It seems like there are hundreds!",
+  author: "Chuck Menkin",
+  experience: "total noob"
+}, {
+  id: 2,
+  body: "Should I buy a Mac or PC to get started?",
+  author: "Alex Sondheim",
+  experience: "some HTML"
+}, {
+  id: 3,
+  body: "Do I need a computer science degree to get a job?",
+  author: "Sarah Rodgers",
+  experience: "made my MySpace cute..."
+}];
+
 export default Ember.Route.extend({
   model() {
     return questions;
